c817d875e24ffcddf97bdce24ed4fc451776e34e
saku <pg.saku0719@gmail.com>
2023-10-19 14:14:25 +0900

    postの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
 import { useState, useEffect } from "react";
 import { Header } from "./Components/Header/Header";
 import { fetchGet } from "./api/fetchGet";
+import { fetchPost } from "./api/fetchPost";
 
 function App() {
-  const [input, setInput] = useState(0);
-  useEffect(() => {
-    const data = fetchGet(10);
-    data.then((data) => console.log("data=", data));
-  }, []);
+  const [getInput, setGetInput] = useState(0);
+  const [postInput, setPostInput] = useState(0);
 
-  const handleChange = (n) => {
-    setInput(n);
+  const getHandleChange = (n) => {
+    setGetInput(n);
   };
 
-  const handleClick = (n) => {
+  const getHandleClick = (n) => {
     const data = fetchGet(Number(n));
+    data.then((data) => console.log("data=", data));
+  };
+
+  const postHandleChange = (n) => {
+    setPostInput(n);
+  };
+
+  const postHandleClick = (n) => {
+    const data = fetchPost("panda", Number(n), "chocolate");
     data.then((data) => console.log("data=", data));
   };
 
   return (
     <>
       <Header />
+      <h1>Hello, World!</h1>
       <div>
-        <h1>Hello, World!</h1>
+        <h2>getのテスト</h2>
         <input
           type="number"
           name="number"
           id="number"
-          onChange={(e) => handleChange(e.target.value)}
+          onChange={(e) => getHandleChange(e.target.value)}
         />
-        <button onClick={() => handleClick(input)}>フェッチ</button>
+        <button onClick={() => getHandleClick(getInput)}>フェッチget</button>
+      </div>
+      <div>
+        <h2>postのテスト</h2>
+        <input
+          type="number"
+          name="number"
+          id="number"
+          onChange={(e) => postHandleChange(e.target.value)}
+        />
+        <button onClick={() => postHandleClick(postInput)}>フェッチpost</button>
       </div>
     </>
   );
