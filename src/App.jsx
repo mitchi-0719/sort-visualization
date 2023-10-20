e24658aaf0d3263aefa4900745ef4814f695e72c
saku <pg.saku0719@gmail.com>
2023-10-20 17:02:30 +0900

    react-sortの追加とお試しの削除

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
-import { useState, useEffect } from "react";
-import { Header } from "./Components/Header";
-import { fetchGet } from "./api/fetchGet";
-import { fetchPost } from "./api/fetchPost";
+import { Header, SortSetting } from "./components";
 import { apiFetch } from "./api/apiFetch";
 
 function App() {
-  const [getInput, setGetInput] = useState(0);
-  const [postInput, setPostInput] = useState(0);
-
-  const getHandleChange = (n) => {
-    setGetInput(n);
-  };
-
-  const getHandleClick = (n) => {
-    const data = fetchGet(Number(n));
-    data.then((data) => console.log("data=", data));
-  };
-
-  const postHandleChange = (n) => {
-    setPostInput(n);
-  };
-
-  const postHandleClick = (n) => {
-    const data = fetchPost("panda", Number(n), "chocolate");
-    data.then((data) => console.log("data=", data));
-  };
 
   return (
     <>
       <Header />
+      <SortSetting />
     </>
   );
 }
